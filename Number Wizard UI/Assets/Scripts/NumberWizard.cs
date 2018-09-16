51f72e6075efd5ddacfbff7e2f4959a0cbd66934
wcalvert88 <wcalvert88@gmail.com>
2018-09-16 18:05:16 -0400

    finished adding functionality to the buttons and refactored the code from the original game

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+    [SerializeField] int max = 1000;
+    [SerializeField] int min = 1;
+    int guess;
+
+	// Use this for initialization
+	void Start () {
+        StartGame();
+    }
+
+    void StartGame ()
+    {
+        max = max + 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+
+    void RestartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        StartGame();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+}
